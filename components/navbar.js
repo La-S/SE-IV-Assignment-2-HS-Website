cc45ae50a73a8f5a45450c87b97af7e5ef3f12fa
Lance Skinner <sirlancelot2604@outlook.com>
2025-08-29 10:11:35 -0500

    add navbar

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+document.write(
+    `
+    <link rel="stylesheet" href="navbar.css">
+
+    <div class='navbar'>
+        <ul>
+        <li><a href='./index.html'>Home</a></li>
+        <li><a href='./skills.html'>How to prepare</a></li>
+        <li><a href='./index.html'>What is it like</a></li>
+        <li><a href='./careers.html'>Careers</a></li>
+        <li><a href='./index.html'>Learn More</a></li>
+        </ul>
+    </div>
+    `
+);
